c2b27d9b90be9e1295b34659e5d3a4ccd986057b
DCA <paquirri@no-molestar.es>
2026-02-19 20:38:02 +0100

    v3.2 en desarrollo

---
+++ src/features/albums/data/AlbumMemLocalSource.java
@@ -0,0 +1,37 @@
+package features.albums.data;
+
+import features.albums.domain.Album;
+import java.util.ArrayList;
+
+public class AlbumMemLocalSource {
+
+    private static AlbumMemLocalDataSource instance = null;
+
+    private ArrayList<Album> storage = new ArrayList<>();
+
+    private AlbumMemLocalDataSource() {
+        initData();
+    }
+
+    private void initData(){
+        Album album = new Album("1","album1", "1999");
+        storage.add(album);
+    }
+
+    public ArrayList<Album> findAll(){
+        return storage;
+    }
+
+    public void save(Album album){
+        storage.add(album);
+        System.out.println(album);
+    }
+
+    public static AlbumMemLocalDataSource newInstance(){
+        if (instance == null){
+            instance = new AlbumMemLocalDataSource();
+        }
+
+        return instance;
+    }
+}
